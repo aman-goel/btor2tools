89d6ceb5ffc0866ae48a0770670c7f11f32261c4
Aman Goel <amangoel@umich.edu>
2019-10-08 19:54:08 -0400

    Corrects output parsing
    Corrects parsing of output (as a unary op)
    
    Removes unnecessary assertion for init

--- src/btor2parser/btor2parser.c
+++ src/btor2parser/btor2parser.c
@@ -1290,8 +1290,8 @@ parse_init_bfr (Btor2Parser *bfr, Btor2Line *l)
   state = id2line_bfr (bfr, l->args[0]);
   if (state->tag != BTOR2_TAG_state)
     return perr_bfr (bfr, "expected state as first argument");
-  if (l->args[0] < labs (l->args[1]))
-    return perr_bfr (bfr, "state id must be greater than id of second operand");
+//  if (l->args[0] < labs (l->args[1]))
+//    return perr_bfr (bfr, "state id must be greater than id of second operand");
   if (!check_state_init (bfr, l->args[0], l->args[1])) return 0;
   if (state->init)
     return perr_bfr (bfr, "state %" PRId64 " initialized twice", state->id);
@@ -1437,7 +1437,7 @@ START:
       PARSE (one, constant);
       PARSE (ones, constant);
       PARSE (or, binary_op);
-      PARSE (output, constraint);
+      PARSE (output, unary_op);
       break;
     case 'r':
       PARSE (read, binary_op);
